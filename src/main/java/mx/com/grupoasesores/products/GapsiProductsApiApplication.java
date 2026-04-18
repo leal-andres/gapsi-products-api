fd4d6037a62f6b03d5f20047ed2ca8d5c203cdc0
Andres Leal <andres.leal@andres-leal.local>
2026-04-18 14:36:33 -0500

    initial repository setup

---
+++ src/main/java/mx/com/grupoasesores/products/GapsiProductsApiApplication.java
@@ -0,0 +1,13 @@
+package mx.com.grupoasesores.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GapsiProductsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GapsiProductsApiApplication.class, args);
+    }
+
+}
